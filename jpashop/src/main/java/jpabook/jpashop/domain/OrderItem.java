179bc863198e46aa56a65a36eb9b814336f1d385
gardening-y <60008786@naver.com>
2023-11-30 21:12:10 +0900

    6. 주문 도메인 개발

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,11 +2,15 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+// service에서 생성하지 못하게 막기 위함 -> 좋은 유지보수 가능
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -24,4 +28,26 @@ public class OrderItem {
     private int orderPrice; // 주문 가격
     private int count; // 주문 수량
 
+//    ==생성 메서드==    //
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+//    ==비즈니스 로직==   //
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+//    ==조회 로직==    //
+
+//    주문상품 전체 가격 조회
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
---
+++ jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,69 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.Item.Item;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+// 비즈니스 로직이 대부분 엔티티에 존재 -> 서비스 계층은 단순히 엔티티에 필요한 요청을 위임하는 역할
+// 엔티티가 비즈니스 로직을 가지고 객체 지향의 특성을 적극 활용하는 것을 도메인 모델 패턴
+// 반대는 트랜잭션 스크립트 패턴
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+//    주문
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+
+//        엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+//        배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+//        주문상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+//        주문 생성
+        Order order = Order.createOrder(member,delivery, orderItem);
+
+//        주문 저장
+        orderRepository.save(order);
+//        order만 저장 (orderitem, delivery은 저장 안됨 -> cascade이고, 다른곳에서 참조하는 것이 없어서 가능)
+
+        return order.getId();
+    }
+
+//    주문 취소
+    @Transactional
+    public void cancelOrder(Long orderId) {
+//        주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+//        주문 취소
+        order.cancel();
+//        JPA 장점 -> 알아서 수량 쿼리가 날라감
+    }
+
+//    검색
+//    public List<Order> findOrder(OrderSearch orderSearch) {
+//        return orderRepository.findAll(orderSearch);
+//    }
+}
